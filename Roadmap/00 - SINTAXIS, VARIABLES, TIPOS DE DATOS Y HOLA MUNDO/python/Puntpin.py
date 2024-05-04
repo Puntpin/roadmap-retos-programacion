151f98d2c436af44836b0a654b107c1c9bcde43a
Puntpin <166245866+Puntpin@users.noreply.github.com>
2024-05-04 10:44:25 -0500

    Create Puntpin.py
    Primer ejercicio del roadmap

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Puntpin.py
@@ -0,0 +1,23 @@
+# https://www.python.org
+
+#Comentario en una linea
+
+"""
+Comentario 
+en bloque
+"""
+'''
+Comentario en 
+bloque tambien
+'''
+
+una_variable = "Keller"
+UNA_CONSTANTE = 3.1416
+
+una_string = "Mi perro es muy lindo"
+un_entero = 12
+un_booleano = True
+un_booleano = False
+un_float = 12.0
+
+print("Hola, Python")
